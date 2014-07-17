d3d52059b5aab2a6515b88fec97ba21dc3669518
nicholasxjy <yitaqi2011@gmail.com>
2014-07-17 22:58:28 +0800

    add some

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ var port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/static'));
 app.use(function(req, res) {
-    res.sendFile('./static/index.html');
+    res.sendfile('./static/index.html');
 });
 
 var io = require('socket.io').listen(app.listen(port));
 
+
+var messages = [];
 io.sockets.on('connection', function(socket) {
-    socket.emit('connected');
+    socket.on('getAllMessages', function() {
+        socket.emit('allMessages', messages);
+    });
+
+    socket.on('createMessage', function(message) {
+        messages.push(message);
+        io.sockets.emit('messageAdd', message);
+    });
 });
 
 console.log('The server is listening on port ' + port);
--- static/chat.js
+++ static/chat.js
@@ -1,37 +1,87 @@
-angular.module('chatApp', ['ngRoute']).
-run(function ($window, $rootScope, $http, $location) {
-    $http({
-        url: 'api/validate',
-        method: 'GET'
-    }).success(function(user) {
-        $rootScope.me = user;
-        $location.path('/');
-    }).error(function(data) {
-        $location.path('/login');
+var app = angular.module('chatApp');
+
+app.factory('socket', function($rootScope){
+    var socket = io.connect('/');
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        }
+    };
+});
+
+app.controller('RoomCtrl', function($scope, socket){
+    $scope.messages = [];
+    socket.emit('getAllMessages');
+    socket.on('allMessages', function(messages) {
+        $scope.messages = messages;
     });
-    $rootScope.logout = function() {
-        $http({
-            url: '/api/logout',
-            method: 'GET'
-        }).success(function() {
-            $rootScope.me = null;
-            $location.path('/login');
-        })
-    }
-    $rootScope.$on('login', function(evt, me) {
-        $rootScope.me = me;
+
+    socket.on('messageAdd', function(message) {
+        $scope.messages.push(message);
     });
 });
 
+app.controller('MessageCreatorCtrl', function($scope, socket) {
+    $scope.newMessage = '';
+    $scope.createMessage = function() {
+        if ($scope.newMessage == '') {
+            return;
+        }
+        socket.emit('createMessage', $scope.newMessage);
+        $scope.newMessage = '';
+    }
+});
 
+app.directive('autoScrollToBottom', function() {
+    return {
+        link: function(scope, element, attrs) {
+            scope.$watch(function() {
+                return element.children.length;
+            }, function() {
+                element.animate({
+                    scrollTop: element.prop('scrollHeight')
+                }, 1000);
+            })
+        }
+    }
+});
 
-
-
-
-
-
-
-
-
-
-
+app.directive('ctrlEnterBreakLine', function() {
+    return function(scope, element, attrs) {
+        var ctrlDown = false;
+        element.bind('keyDown', function(evt) {
+            if (evt.which == 17) {
+                ctrlDown = true;
+                setTimeOut(function() {
+                    ctrlDown = false;
+                }, 1000);
+            }
+            if (evt.which == 13) {
+                if (ctrlDown) {
+                    element.val(element.val() + '\n');
+                } else {
+                    scope.$apply(function() {
+                        scope.$eval(attrs.ctrlEnterBreakLine);
+                    });
+                    evt.preventDefault();
+                }
+            }
+        });
+    }
+});
